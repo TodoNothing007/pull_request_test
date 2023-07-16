13ae9af0436437f60268c9c177f79c3407c6a142
957589005 <957589005@qq.com>
2023-07-16 20:01:14 +0800

    更新完整的快排函数代码

--- sort.c
+++ sort.c
@@ -30,6 +30,32 @@ void quickSort(int arr[], int low, int high)
     }
 }
 
+// 快速排序辅助函数，用于找到基准元素的位置并对数组进行分割
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high]; // 选择数组的最后一个元素作为基准元素
+    int i = low - 1;
+
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            // 交换arr[i]和arr[j]的值
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    // 将基准元素放置在正确的位置
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return i + 1; // 返回基准元素的位置
+}
+
 // 测试排序函数
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
